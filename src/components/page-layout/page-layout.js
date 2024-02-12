43f96dd3666d320134bade6341a1cb347e5367d8
nblyumenshteyn <nblyumenshteyn@gmail.com>
2024-02-12 21:05:16 +0400

    add page-layout

---
+++ src/components/page-layout/page-layout.js
@@ -0,0 +1,21 @@
+import {memo} from "react";
+
+function PageLayout() {
+
+  return (
+    <div>
+      {/* <div>
+        <Header />
+      </div>
+      <div>
+        <Main />
+      </div>
+      <div>
+        <Footer />
+      </div> */}
+
+    </div>
+  );
+}
+
+export default memo(PageLayout);
